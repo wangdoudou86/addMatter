981215882db3e819ba98616eacea1e2beff10e4c
krystal <892184375@qq.com>
2019-03-28 02:29:49 +0800

    创建项目

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from './views/Login.vue'
 import Home from './views/Home.vue'
 
 Vue.use(Router)
@@ -10,16 +11,32 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'login',
+      component: Login
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/home',
+      name: 'home',
+      component: Home,
+      children:[{
+        path: 'list',
+        name: 'list',
+        component: () => import(/* webpackChunkName: "list" */ './views/List.vue')
+          },
+        {
+          path: 'admin',
+          name: 'admin',
+          component: () => import(/* webpackChunkName: "admin" */ './views/Admin.vue') //用懒加载
+          }
+      ]
     }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // }
   ]
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <router-view/>
+    <ul>
+        <li><router-link :to="{name:'list'}">事项列表</router-link></li>
+        <li><router-link :to="{name:'admin'}">添加事项</router-link></li>
+    </ul> 
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
+<style lang="sass" scoped>
+
+</style>
